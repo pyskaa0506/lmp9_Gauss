ff6099a0d915007d7ad7e389d00ccdd502ca0266
pyskaa0506 <62877223+pyskaa0506@users.noreply.github.com>
2024-01-02 13:58:51 +0100

    poprawki backsubst i dodanie funkcji eliminate

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,7 +4,7 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) 
+int  backsubst(Matrix *x, Matrix *mat, Matrix *b)
 {
 	int n = mat -> r;
 	double sum = 0.0;
@@ -15,27 +15,28 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b)
 		/*
 		mat->r != mat->c czy liczba wierszy (r) nie jest równa liczbie kolumb (c); tzn - macierz nie jest kwadratowa
 		x->r != mat->r czy liczba wrs mac. X nie równa lb. wiersz. mac. Mat
-		b->c != 1 czy liczba kolumn mac. b nie równa 1 
+		b->c != 1 czy liczba kolumn mac. b nie równa 1
 		b->r != mat->r czy liczba wier. mac. b nie równa lb. lb. wiers. mac. Mat
-		x->c != 1 czy lb. kolumn mac. X nie równa 1 
+		x->c != 1 czy lb. kolumn mac. X nie równa 1
 		*/
 		return 2;
 	}
 
-	for (int i = n-1; i >= 0; i--) //iteruje macierz od ostatniego do pierwszego wiersza
-	{
-		for (int j = i+1; j <n; j++) //iteruje po kolumnach 
-		{
-			sum += mat -> data [i][j]*x->data[j][0]; //suma iloczynów współczynników i odpowiednich wartości x
-		}
+    for (int i = n-1; i >= 0; i--) //iteruje macierz od ostatniego do pierwszego wiersza
+    {
+        double sum = 0.0; // Zerowanie sumy dla każdego wiersza
 
-		if (mat -> data[i][j] == 0.0) //czy element przekątnej wynosi 0
-		{
-			return 1;
-		}
-		// obliczanie rozwiązania x dla pewnego (bieżącego) wiersza za pomocą podstawienia wstecznego
-		x -> data[i][0] = (b -> data[i][0] - sum ) / mat -> data[i][i]; 
-	}
+        for (int j = i + 1; j < n; j++) {  //iteruje po kolumnach
+            sum += mat->data[i][j] * x->data[j][0]; //suma iloczynów współczynników i odpowiednich wartości x
+        }
+
+        if (mat -> data[i][i] == 0.0) { //czy element przekątnej wynosi 0
+            return 1;
+        }
+    // obliczanie rozwiązania x dla pewnego (bieżącego) wiersza za pomocą podstawienia wstecznego
+        x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+    }
+
 
 	return 0;
 }
--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,19 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+    int i,j,k;
+    double mnoznik;
+    for(i=0;i<mat->r;i++){
+        for(j=i+1;j<mat->r;j++){
+            if(mat->data[i][i]==0){
+                return 1;
+            }
+            mnoznik=mat->data[j][i]/mat->data[i][i];
+            for(k=i;k<mat->c;k++){
+                mat->data[j][k]=mat->data[j][k]-mnoznik*mat->data[i][k];
+            }
+            b->data[j][0]=b->data[j][0]-mnoznik*b->data[i][0];
+        }
+    }
+    return 0;
 }
-
