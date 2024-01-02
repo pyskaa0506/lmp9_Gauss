0a13fef2f2504930f7b5c1f574e0eef37832bab9
pyskaa0506 <62877223+pyskaa0506@users.noreply.github.com>
2024-01-02 14:43:38 +0100

    modyfikacja eliminate

--- src/gauss.c
+++ src/gauss.c
@@ -1,4 +1,5 @@
 #include "gauss.h"
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -7,8 +8,34 @@
 int eliminate(Matrix *mat, Matrix *b){
     int i,j,k;
     double mnoznik;
+    double maxVal;
+    int maxIdx;
+
     for(i=0;i<mat->r;i++){
+        maxVal = fabs(mat->data[i][i]);
+        maxIdx = i;
+
         for(j=i+1;j<mat->r;j++){
+            if(fabs(mat->data[j][i]) > maxVal){
+                maxVal = fabs(mat->data[j][i]);
+                maxIdx = j;
+            }
+        }
+
+
+        if(maxIdx != i){
+            for(k=i; k<mat->c; k++){
+                double temp = mat->data[i][k];
+                mat->data[i][k] = mat->data[maxIdx][k];
+                mat->data[maxIdx][k] = temp;
+            }
+
+            double tempB = b->data[i][0];
+            b->data[i][0] = b->data[maxIdx][0];
+            b->data[maxIdx][0] = tempB;
+        }
+
+        for(j=i+1; j<mat->r; j++){
             if(mat->data[i][i]==0){
                 return 1;
             }
@@ -21,3 +48,4 @@ int eliminate(Matrix *mat, Matrix *b){
     }
     return 0;
 }
+
